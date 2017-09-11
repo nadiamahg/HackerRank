40bebf6ec8943147fdf1311f2e815e2f04c24607
Nadia <nadia.mahgerefteh.15@ucl.ac.uk>
2017-09-11 15:42:44 +0100

    warmup 8

--- C/Algorithms/WarmupChallenges.c
+++ C/Algorithms/WarmupChallenges.c
@@ -211,4 +211,37 @@ int main() {
 
 ----------------------------------------------------------
 
+----------------------------------------------------------
+8. Birthday Cake Candles
+
+int birthdayCakeCandles(int n, int ar_size, int* ar) {
+    int max = 0;
+    int result = 0;
+    for(int x = 0; x < ar_size; x ++) {
+        max = (ar[x] > max ? ar[x] : max);
+    } 
+    for (int y = 0; y < ar_size; y ++) {
+        if(ar[y] == max) {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n; 
+    scanf("%i", &n);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       scanf("%i",&ar[ar_i]);
+    }
+    int result = birthdayCakeCandles(n, n, ar);
+    printf("%d\n", result);
+    return 0;
+}
+----------------------------------------------------------
+
+----------------------------------------------------------
+
+----------------------------------------------------------
 
