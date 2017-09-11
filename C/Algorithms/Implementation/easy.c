f5222767a2f28a09ed7e1b6a18e30aac98adbd5a
Nadia <nadia.mahgerefteh.15@ucl.ac.uk>
2017-09-11 20:52:11 +0100

    implentation easy 3

--- C/Algorithms/Implementation/easy.c
+++ C/Algorithms/Implementation/easy.c
@@ -93,3 +93,37 @@ int main(){
 }
 
 ------------------------------------
+
+------------------------------------
+3. Kangaroo
+
+char* kangaroo(int x1, int v1, int x2, int v2) {
+    
+    if (x1 == x2) {
+        return "YES";
+    } else if (v2 == v1) {
+        return "NO";
+    } else {
+        int a = ((x1 - x2) / (v2 - v1));
+        if(a >= 0 && (((x1 - x2) % (v2 - v1)) == 0 )){
+            return "YES";
+        } 
+    }
+    
+    return "NO";
+}
+
+int main() {
+    int x1; 
+    int v1; 
+    int x2; 
+    int v2; 
+    scanf("%i %i %i %i", &x1, &v1, &x2, &v2);
+    int result_size;
+    char* result = kangaroo(x1, v1, x2, v2);
+    printf("%s\n", result);
+    return 0;
+}
+
+
+------------------------------------
