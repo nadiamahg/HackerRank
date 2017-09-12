cc1322833814ab8281b3ed662d95d32c75944f42
Nadia <nadia.mahgerefteh.15@ucl.ac.uk>
2017-09-12 23:47:02 +0100

    implementation 9

--- C/Algorithms/Implementation/easy.c
+++ C/Algorithms/Implementation/easy.c
@@ -176,6 +176,7 @@ int main() {
 
 
 ------------------------------------
+------------------------------------
 5. Breaking the Records
 
 int* getRecord(int ssize, int* s, int *result_size){
@@ -259,5 +260,50 @@ int main() {
     return 0;
 }
 
+------------------------------------
+------------------------------------
+9. Divisible Sum Pairs
+
+int divisibleSumPairs(int n, int k, int arsize, int* ar) {
+    
+    int sum = 0;
+    int result = 0;
+    int i;
+    int j;
+    
+    for(int x = 0; x < arsize; x++) {
+        i = ar[x];
+        if(x+1 < arsize){
+            for(int y = x+1; y < arsize; y++) {
+                j = ar[y];
+                sum = i + j;
+                if(sum % k == 0){
+                    result ++;
+                }
+            sum = 0;
+            }
+        }
+        
+    }
+        
+     return result;
+}
+
+int main() {
+    int n; 
+    int k; 
+    scanf("%i %i", &n, &k);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       scanf("%i",&ar[ar_i]);
+    }
+    int result = divisibleSumPairs(n, k, n, ar);
+    printf("%d\n", result);
+    return 0;
+}
+
+------------------------------------
+------------------------------------
+10. 
 ------------------------------------
 ------------------------------------
