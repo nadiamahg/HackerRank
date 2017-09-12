fb1eb3a7635deca3fa5d663d54da9d531fc321fa
Nadia <nadia.mahgerefteh.15@ucl.ac.uk>
2017-09-12 21:09:46 +0100

    day 1 swift

---
+++ 30 Days of Code/Day 1- Data Types/DataTypes.swift
@@ -0,0 +1,23 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+
+var a: Int
+var b: Double
+var c: String
+
+// Read and save an integer, double, and String to your variables.
+a = Int(readLine()!)!
+b = Double(readLine()!)!
+c = readLine()!
+
+// Print the sum of both integer variables on a new line.
+// Print the sum of the double variables on a new line.
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(i+a)
+print(d+b)
+print(s+c)
